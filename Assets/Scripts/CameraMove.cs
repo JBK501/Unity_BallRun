bea358ed7f8448e2f1a3d2c7613d650df1a6cf2d
Hyeontae An <70844171+JBK501@users.noreply.github.com>
2023-04-12 16:35:59 +0900

    ADD BallRun
    공굴리기 프로젝트 추가

---
+++ Assets/Scripts/CameraMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+
+    Transform playerTransform;
+    Vector3 Offset;
+
+    void Awake()
+    {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        Offset = transform.position - playerTransform.position; // 카메라와 플레이어가 떨어진 위치
+    }
+
+  
+    void LateUpdate()
+    {
+        // 플레이어가 이동할 때마다 카메라 위치 조정
+        transform.position = playerTransform.position + Offset;
+    }
+}
---
+++ Assets/Scripts/PlayerBall.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerBall : MonoBehaviour
+{
+    public float jumpPower;
+    public int itemCount;
+    public GameManagerLogic manager;
+
+    bool isJump;
+
+    Rigidbody rigid;
+    AudioSource audioSource;
+
+    private void Awake()
+    {
+        isJump = false;
+        rigid = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        // 점프
+        if(Input.GetButtonDown("Jump") && !isJump)
+        {
+            isJump = true;
+            rigid.AddForce(new Vector3(0, jumpPower, 0),ForceMode.Impulse);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal"); // 수평이동
+        float v = Input.GetAxisRaw("Vertical"); // 수직이동
+
+        rigid.AddForce(new Vector3 (h,0,v),ForceMode.Impulse);
+    }
+
+    // 바닥 충돌 시 점프 플래그 변경
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Floor")
+            isJump = false;
+    }
+
+
+    // 아이템 먹을 시 개수 추가
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Item")
+        {
+            itemCount++;
+            audioSource.Play();
+            other.gameObject.SetActive(false);
+            manager.GetItem(itemCount);
+        }
+        else if(other.tag == "Point")
+        {
+            if(itemCount == manager.totalItemCount)
+            {
+                // Game Clear
+                SceneManager.LoadScene(manager.stage+1);
+            }
+            else
+            {
+                // Restart
+                SceneManager.LoadScene(manager.stage);
+            }
+        }
+    }
+}
